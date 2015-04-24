aecaa0eba7ec1126536acec4f13ee262d12bddf9
Sebastian Mahlke <smahlke@me.com>
2015-04-24 16:12:33 +0200

    First Commit.

---
+++ my-first-app/app/controllers/Application.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.twirl.api.Html;
+import views.html.*;
+
+public class Application extends Controller {
+
+    public static Result index() {
+        return ok(index.render("Your new application is reayd."));
+    }
+    
+    public static Result main() {
+        return ok(main.render("Some Title", Html.apply("<span> asdf </span>")));
+    }
+    
+    public static Result trivial(String name) {
+        return ok("Hello " + name);
+    }
+}
